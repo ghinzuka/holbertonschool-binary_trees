09b0269a25bbac4db27477fa5c2700f49870de07
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-08 12:13:50 +0200

    using two recursive to go to the bottom (leaf) and recursivly free tree

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child an other node
+ *
+ * @parent: pointer to the parent.
+ * @value: value
+ *
+ * Return: return a pointer to the created node,
+ * or NULL on failure or if parent is NULL
+*/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	binary_tree_t *new = binary_tree_node(parent, value);
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
+		new->right->parent = new;
+	}
+	parent->right = new;
+	return (new);
+}
