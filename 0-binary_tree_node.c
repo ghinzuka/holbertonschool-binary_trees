fc279778c3035ce801d1190195e358e2e58ce101
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-08 10:18:36 +0200

    creating function same as a linked node but with a parents instead of prev and two child as next

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,4 +9,18 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 
+	if (value == '\0')
+		return (NULL);
+
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+
 }
