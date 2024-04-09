42f5d3780fb015500b49cddcc51b06d440a224f6
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-09 12:15:54 +0200

    using same function but adding a level to each statement

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle -  finds the uncle of a node
+ * @node: nods to check uncle
+ * Return: pointer to uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->parent->parent->right == node->parent)
+	{
+		return (node->parent->parent->left);
+	}
+	else
+	{
+		return (node->parent->parent->right);
+	}
+}
