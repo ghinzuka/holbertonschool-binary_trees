471e41636723647d010c26dae7c36438cd33f874
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-09 12:32:15 +0200

    changin for betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,24 +1,18 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_uncle - function that finds the uncle of a node
- *
- * @node: a pointer to the node to find the uncle.
- *
- * Return: Null or the uncle.
-*/
-
+ * binary_tree_uncle -  finds the uncle of a node
+ * @node: nods to check uncle
+ * Return: pointer to uncle node
+ */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	if (node == NULL || node->parent->parent == NULL || node->parent == NULL)
 	{
 		return (NULL);
 	}
-
-	if (node->parent->parent->left == node->parent)
+	if (node->parent->parent->right == node->parent)
 	{
-		return (node->parent->parent->right);
+		return (node->parent->parent->left);
 	}
-
-	return (node->parent->parent->left);
+		return (node->parent->parent->right);
 }
