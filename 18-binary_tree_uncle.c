f80d81a8d00ba889a8f34fca66fa879962672d7c
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-09 12:25:25 +0200

    changing left and right statement

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,12 +10,10 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	{
 		return (NULL);
 	}
-	if (node->parent->parent->left == node->parent)
-	{
-		return (node->parent->parent->right);
-	}
-	else
+	if (node->parent->parent->right == node->parent)
 	{
 		return (node->parent->parent->left);
 	}
+
+		return (node->parent->parent->right);
 }
